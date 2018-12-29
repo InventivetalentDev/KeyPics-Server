556ee1390c9cae05ee0f945be7f187c98cf93c89
inventivetalent <mail@inventivetalent.org>
2018-12-29 21:17:07 +0100

    move label drawing function to utils

--- routes/key.js
+++ routes/key.js
@@ -6,15 +6,9 @@ const TextToSVG = require("text-to-svg");
 const Color = require("color");
 const util = require("../util");
 
-const faCore = require('@fortawesome/fontawesome-svg-core');
-const faRegular = require('@fortawesome/free-regular-svg-icons');
-const faSolid = require('@fortawesome/free-solid-svg-icons');
-const faBrands = require('@fortawesome/free-brands-svg-icons');
 
 module.exports = function (express, config) {
     let router = express.Router();
-
-    faCore.library.add(faRegular.far, faSolid.fas, faBrands.fab);
 
     router.get("/:label*(\.:ext)?", (req, res) => {
         let start = Date.now();
@@ -93,59 +87,17 @@ module.exports = function (express, config) {
                 draw.select(".front_line").stroke(targetColor.lighten(0.5).hex());
             }
 
-            // Load font
-            let fontFile = path.join(cwd(), "assets/fonts/", fontFamily, fontFamily + "-" + fontStyle + ".ttf");
+            util.drawLabel(draw, label, fontFamily, fontStyle, fontSize, labelColor, res)
+                .then(() => {
+                    let svgString = draw.node.outerHTML;
+                    // Clear when done
+                    draw.clear();
 
-
-            function svgDone() {
-                let svgString = draw.node.outerHTML;
-                // Clear when done
-                draw.clear();
-
-                res.set({
-                    "X-Gen-Duration": (Date.now() - start)
-                });
-                util.sendImage(svgString, ext, res);
-            }
-
-            if (label.startsWith("far:") || label.startsWith("fas:") || label.startsWith("fab:")) {
-                let split = label.split(":");
-                let prefix = split[0];
-                let iconName = split[1];
-
-                let def = faCore.findIconDefinition({prefix: prefix, iconName: iconName});
-                if (!def) {
-                    res.status(400).send("Unknown icon " + prefix + ":" + iconName);
-                    return;
-                }
-                let path = draw.path(def.icon[4]);
-                path.fill(labelColor);
-                path.size(fontSize);
-                path.cx(width / 2).cy(height / 2);
-
-                svgDone();
-            } else {
-                TextToSVG.load(fontFile, function (err, textToSvg) {
-                    if (err) {
-                        res.status(400).send("Failed to load font " + fontFamily + " " + fontStyle);
-                        return console.warn(err);
-                    }
-
-                    let textPath = textToSvg.getPath(label, {
-                        x: width / 2,
-                        y: height / 2,
-                        fontSize: fontSize,
-                        anchor: "center middle",
-                        attributes: {
-                            fill: labelColor
-                        }
+                    res.set({
+                        "X-Gen-Duration": (Date.now() - start)
                     });
-                    draw.svg(textPath);
-
-                    svgDone();
-                });
-            }
-
+                    util.sendImage(svgString, ext, res);
+                })
 
         });
 
--- util.js
+++ util.js
@@ -1,4 +1,13 @@
 const svg2img = require("svg2img");
+const TextToSVG = require("text-to-svg");
+const cwd = require("process").cwd;
+
+const faCore = require('@fortawesome/fontawesome-svg-core');
+const faRegular = require('@fortawesome/free-regular-svg-icons');
+const faSolid = require('@fortawesome/free-solid-svg-icons');
+const faBrands = require('@fortawesome/free-brands-svg-icons');
+
+faCore.library.add(faRegular.far, faSolid.fas, faBrands.fab);
 
 function initNewSvg(width,height) {
     let window = require('svgdom');
@@ -10,6 +19,52 @@ function initNewSvg(width,height) {
         draw.size(width, height);
     }
     return draw;
+}
+
+function drawLabel(draw, label, fontFamily, fontStyle, fontSize, labelColor,res) {
+   return new Promise((resolve,reject)=>{
+       if (label.startsWith("far:") || label.startsWith("fas:") || label.startsWith("fab:")) {
+           let split = label.split(":");
+           let prefix = split[0];
+           let iconName = split[1];
+
+           let def = faCore.findIconDefinition({prefix: prefix, iconName: iconName});
+           if (!def) {
+               res.status(400).send("Unknown icon " + prefix + ":" + iconName);
+               reject();
+               return;
+           }
+           let path = draw.path(def.icon[4]);
+           path.fill(labelColor);
+           path.size(fontSize);
+           path.cx(width / 2).cy(height / 2);
+
+           resolve();
+       } else {
+           // Load font
+           let fontFile = path.join(cwd(), "assets/fonts/", fontFamily, fontFamily + "-" + fontStyle + ".ttf");
+           TextToSVG.load(fontFile, function (err, textToSvg) {
+               if (err) {
+                   res.status(400).send("Failed to load font " + fontFamily + " " + fontStyle);
+                   reject(err);
+                   return console.warn(err);
+               }
+
+               let textPath = textToSvg.getPath(label, {
+                   x: width / 2,
+                   y: height / 2,
+                   fontSize: fontSize,
+                   anchor: "center middle",
+                   attributes: {
+                       fill: labelColor
+                   }
+               });
+               draw.svg(textPath);
+
+               resolve();
+           });
+       }
+   })
 }
 
 function sendImage(svgString, ext, res) {
@@ -32,4 +87,4 @@ function sendImage(svgString, ext, res) {
     }
 }
 
-module.exports = {initNewSvg, sendImage};
+module.exports = {initNewSvg, drawLabel, sendImage};
